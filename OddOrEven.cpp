22f86df26bd734d0d1c3d491c84a92903b352554
SatvikPrabhu <satvikp.567@gmail.com>
2025-11-20 13:17:06 +0530

    Odd or even function in c++

---
+++ OddOrEven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<< "Enter a number n: ";
+    int n;
+    cin>>n;
+    if(n%2 == 0){
+        cout<< n << " is even"<<endl;
+
+    }else{
+        cout<< n << " is odd"<<endl;
+    }
+
+
+}
